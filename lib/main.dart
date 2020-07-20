c0cfb9956fb3af75ea89033400c8812a2f82473d
Amose <amose.suwali@gmail.com>
2020-07-20 16:34:59 +0200

    Amose Test Push

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,8 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => new _MyAppState();
 }
 
+//Amose Test Push
+
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
